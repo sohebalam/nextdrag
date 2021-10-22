d57cc00b7a8e2e1666173c90356eb78c11e225c8
Soheb Alam <sohebalam@hotmail.com>
2021-10-22 19:19:46 +0100

    first

---
+++ pages/_document.js
@@ -0,0 +1,58 @@
+// import Document, { Html, Head, Main, NextScript } from "next/document"
+// import { extractCritical } from "@emotion/server"
+// import { resetServerContext } from "react-beautiful-dnd"
+
+// export default class MyDocument extends Document {
+//   static async getInitialProps(ctx) {
+//     const initialProps = await Document.getInitialProps(ctx)
+//     resetServerContext()
+//     return { ...initialProps }
+//   }
+
+//   render() {
+//     return (
+//       <Html lang="en">
+//         <Head>
+//           <style
+//             data-emotion-css={this.props.ids.join(" ")}
+//             dangerouslySetInnerHTML={{ __html: this.props.css }}
+//           />
+//         </Head>
+//         <body>
+//           <Main />
+//           <NextScript />
+//         </body>
+//       </Html>
+//     )
+//   }
+// }
+import Document, { Html, Head, Main, NextScript } from "next/document"
+import { extractCritical } from "@emotion/server"
+import { resetServerContext } from "react-beautiful-dnd"
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const page = await ctx.renderPage()
+    const initialProps = await Document.getInitialProps(ctx)
+    const styles = extractCritical(page.html)
+    resetServerContext()
+    return { ...initialProps, ...page, ...styles }
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <style
+            data-emotion-css={this.props.ids.join(" ")}
+            dangerouslySetInnerHTML={{ __html: this.props.css }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
